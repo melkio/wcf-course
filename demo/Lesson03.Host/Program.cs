540742e5ed82969d3b1750f2a9ea2f1fc606c75b
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2013-10-01 17:28:57 +0200

    Lesson 03 completed

---
+++ demo/Lesson03.Host/Program.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using Services;
+using System;
+using System.ServiceModel;
+
+namespace Lesson03.Host
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            using (var host = new ServiceHost(typeof(CalculatorImpl)))
+            {
+                host.AddServiceEndpoint(
+                    typeof(ICalculator),
+                    new NetTcpBinding(),
+                    "net.tcp://localhost:8080/calculator");
+                host.Extensions.Add(new ServiceHostLoggingExtension());
+                host.Open();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ demo/Lesson03.Host/ServiceHostLoggingExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Lesson03.Host
+{
+    class ServiceHostLoggingExtension : IExtension<ServiceHostBase>
+    {
+        public void Attach(ServiceHostBase owner)
+        {
+            owner.Opening += HandleOpening;
+            owner.Opened += HandleOpened;
+            owner.Closing += HandleClosing;
+            owner.Closed += HandleClosed;
+        }
+
+        public void Detach(ServiceHostBase owner)
+        {
+            owner.Opening -= HandleOpening;
+            owner.Opened -= HandleOpened;
+            owner.Closing -= HandleClosing;
+            owner.Closed -= HandleClosed;
+        }
+
+        private void HandleOpening(Object sender, EventArgs e)
+        {
+            Console.WriteLine("Opening service host...");
+        }
+
+        private void HandleOpened(Object sender, EventArgs e)
+        {
+            Console.WriteLine("Service host opened...");
+        }
+
+        private void HandleClosing(Object sender, EventArgs e)
+        {
+            Console.WriteLine("Closing service host...");
+        }
+
+        private void HandleClosed(Object sender, EventArgs e)
+        {
+            Console.WriteLine("Service host closed...");
+        }
+    }
+}
