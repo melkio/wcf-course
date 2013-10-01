133e9b93a403b6ba45c03a7fbc3a5cd5301554bc
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2013-10-01 17:06:23 +0200

    lesson 02 completed

---
+++ demo/Lesson02.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson02.Client
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            using (var client = new CalculatorClient())
+                Console.WriteLine(client.Add(10, 20));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ demo/Lesson02.Host/Program.cs
@@ -0,0 +1,19 @@
+﻿using Services;
+using System;
+using System.ServiceModel;
+
+namespace Lesson02.Host
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            using (var host = new ServiceHost(typeof(CalculatorImpl)))
+            {
+                host.Open();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
